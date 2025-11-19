600bb3cd5646220d8cf2cf22dfa6bc1f20f57328
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-11-19 22:19:30 +0000

    座標圧縮の解説

--- contest/abc213/c/main.py
+++ contest/abc213/c/main.py
@@ -8,10 +8,12 @@ for _ in range(n):
     x.append(a)
     y.append(b)
 
+# 座標圧縮するために、重複を排除してソートしたリストを作成
 rows = sorted((set(x)))
 columns = sorted((set(y)))
 
 for i in range(n):
+    # 座標圧縮、何番目の要素となるか調べる
     compressed_x = bisect.bisect_left(rows, x[i]) + 1
     compressed_y = bisect.bisect_left(columns, y[i]) + 1
     print(compressed_x, compressed_y)
