396b45534cc3d7ba9991faf1f9479477f88f9a75
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-11-27 23:05:04 +0000

    abc220 c

--- contest/abc220/c/main.py
+++ contest/abc220/c/main.py
@@ -2,10 +2,16 @@ N = int(input())
 A = list(map(int, input().split()))
 X = int(input())
 
-current_sum = 0
-count = 0
+# TLEにならないようにひとつずつ要素を見るのではなく、XをAの和で割って商を求めるとANS-1のcountが出る
 
-for i in range(1000000000):
+# qS ≤ X < (q+1)Sになる
+q = X // sum(A)
+
+current_sum = q * sum(A)
+count = q * N
+
+# あとはXを超えるまで足し続ける
+for i in range(N):
     current_sum += A[i % N]
     count += 1
     if current_sum > X:
